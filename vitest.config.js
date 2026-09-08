5d192245fa858ede14e21e56e9b5ec28ce00c896
Omkar Chavan <meomkarchavan99@gmail.com>
2026-09-08 17:10:48 +0530

    chore: remove env from vitest.config.js; rely on standard .env and CI test setup

--- vitest.config.js
+++ vitest.config.js
@@ -8,12 +8,5 @@ export default defineConfig({
     globals: true,
     setupFiles: ['./tests/setup.js'],
     include: ['tests/unit/**/*.{test,spec}.{js,jsx}'],
-    env: {
-      VITE_FIREBASE_API_KEY: 'test-api-key',
-      VITE_FIREBASE_PROJECT_ID: 'eatlog-dev',
-      VITE_FIREBASE_AUTH_DOMAIN: 'eatlog-dev.firebaseapp.com',
-      VITE_FIREBASE_STORAGE_BUCKET: 'eatlog-dev.firebasestorage.app',
-      VITE_FIREBASE_APP_ID: '1:634671381722:web:800fded24bfed2e9f579e3',
-    },
   },
 });
